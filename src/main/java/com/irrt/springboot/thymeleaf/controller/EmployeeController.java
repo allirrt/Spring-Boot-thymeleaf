302019fe35f5ea61d40e06f12dff0a6b9bf2d919
IRRT <zindgg@gmail.com>
2021-02-26 13:01:10 +0300

    Spring-Boot-thymeleaf project / ADD-UPDATE-DELETE

--- src/main/java/com/irrt/springboot/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/irrt/springboot/thymeleaf/controller/EmployeeController.java
@@ -43,7 +43,7 @@ public class EmployeeController {
         return "/employees/employee-form";
     }
 
-    @PostMapping("/showFormForUpdate")
+    @GetMapping("/showFormForUpdate")
     public String showFormForUpdate(@RequestParam("employeeId") int theId,
                                     Model theModel) {
 
@@ -69,4 +69,15 @@ public class EmployeeController {
     }
 
 
+    @GetMapping("/delete")
+    public String delete(@RequestParam("employeeId") int theId) {
+
+        // delete the employee
+        employeeService.deleteById(theId);
+
+        // redirect to /employees/list
+        return "redirect:/employees/list";
+
+    }
 }
+
